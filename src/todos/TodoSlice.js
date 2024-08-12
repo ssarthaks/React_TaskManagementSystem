330e97b91c40159bb021a2866768ce7cf39395f5
ssarthaks <ssarthakxd@gmail.com>
2024-08-12 12:25:18 +0545

    Final commit

--- src/todos/TodoSlice.js
+++ src/todos/TodoSlice.js
@@ -7,56 +7,56 @@ const initialState = {
       id: 1,
       name: "Learning React",
       desc: "Today I will be making a task management system using react redux and redux toolkit.",
-      date: "01-12-2024",
+      date: "2024-01-12", // Updated date format
       completed: false,
     },
     {
       id: 2,
       name: "Learning Tailwind",
       desc: "Tomorrow I will learn TailwindCSS.",
-      date: "01-13-2024",
+      date: "2024-01-13", // Updated date format
       completed: true,
     },
     {
       id: 3,
       name: "Learning Node",
       desc: "Then after tomorrow I will learn Node.",
-      date: "01-14-2024",
+      date: "2024-01-14", // Updated date format
       completed: false,
     },
     {
       id: 4,
       name: "Reading JavaScript Book",
       desc: "Read chapters 3 to 5 of the JavaScript book.",
-      date: "01-15-2024",
+      date: "2024-01-15", // Updated date format
       completed: false,
     },
     {
       id: 5,
       name: "Complete Redux Tutorial",
       desc: "Finish the Redux tutorial on the official website.",
-      date: "01-16-2024",
+      date: "2024-01-16", // Updated date format
       completed: true,
     },
     {
       id: 6,
       name: "Set Up GitHub Repository",
       desc: "Create and initialize a new GitHub repository for the project.",
-      date: "01-17-2024",
+      date: "2024-01-17", // Updated date format
       completed: false,
     },
     {
       id: 7,
       name: "Attend Web Development Workshop",
       desc: "Participate in the online workshop for web development.",
-      date: "01-18-2024",
+      date: "2024-01-18", // Updated date format
       completed: false,
     },
     {
       id: 8,
       name: "Write Blog Post",
       desc: "Draft and publish a blog post about the latest tech trends.",
-      date: "01-19-2024",
+      date: "2024-01-19", // Updated date format
       completed: false,
     },
   ],
@@ -69,11 +69,12 @@ export const todoSlice = createSlice({
   reducers: {
     // Action to add a new task
     addTask: (state, action) => {
-      const { name, desc, completed } = action.payload; // Destructure the payload
+      const { name, desc, date, completed } = action.payload; // Destructure the payload
       const todo = {
         id: nanoid(), // Generate a unique ID for the new task
         name,
         desc,
+        date,
         completed,
       };
       state.todos.push(todo); // Add the new task to the todos array
